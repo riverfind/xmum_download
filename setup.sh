58f99aae07ffbef3da0a78622a8f30072b1b1cde
riverfind <wkj521aaa@gmail.com>
2025-06-19 17:53:40 +0800

    revise
    avoid create environment in current working directory

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 python -m venv $SCRIPT_DIR/.venv
-.venv/bin/pip install -r $SCRIPT_DIR/requirements.txt
+$SCRIPT_DIR/.venv/bin/pip install -r $SCRIPT_DIR/requirements.txt
 echo "init..."
 echo -n "ACCOUNT:"
 read ac
