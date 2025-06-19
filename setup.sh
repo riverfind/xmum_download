f99e3f53c043f1ec62b9c06ca5269300b06f0bb8
riverfind <wkj521aaa@gmail.com>
2025-06-19 17:04:35 +0800

    linux setup support

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+python -m venv .venv
+.venv/bin/pip install -r requirements.txt
+echo "init..."
+echo -n "ACCOUNT:"
+read ac
+echo -n "PASSWORD:"
+read ps
+echo "ac=$ac" >./.env
+echo "ps=$ps" >>./.env
